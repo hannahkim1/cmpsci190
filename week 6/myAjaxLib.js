4e9184f023469fbcfe9bfb68d8ed93865870b906
Hannah Kim <xhannahkim1@gmail.com>
2023-05-20 23:20:46 -0700

    lesson 17 update

---
+++ week 6/myAjaxLib.js
@@ -0,0 +1,60 @@
+function getXMLHttpRequest() {
+    try {
+        try {
+            return new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        catch(e) {
+            return new ActiveXObject("Msxml2.XMLHTTP");
+        }
+    }
+    catch(e) {
+        return new XMLHttpRequest();
+    }
+}
+
+function doAjax(url, query, callback, reqtype, getxml) {
+    var myreq = getXMLHttpRequest();
+
+    var message = document.createElement("div");
+
+    message.innerHTML = "Waiting for an Ajax request to complete";
+    document.body.appendChild(message);
+
+    myreq.onreadystatechange = function() {
+        if (myreq.readyState == 4) {
+            if (myreq.status == 200) {
+                var item = myreq.responseText;
+                if(getxml == 1) {
+                    item = myreq.responseXML;
+                }
+                
+                eval(callback + ('item'));
+                document.body.removeChild(message);
+            }
+        }
+    }
+    if(reqtype.toUpperCase() == "POST") {
+        requestPOST(url, query, myreq);
+    } else {
+        XMLHttpRequestEventTarget(url, query, myreq);
+    }
+}
+
+function requestGET(url, query, req) {
+    var myRandom = parseInt(Math.random() * 99999999);
+    if(query == '') {
+        var callUrl = url + '?rand=' + myRandom;
+    }
+    else {
+        var callUrl = url + '?' + query + '?rand=' + myRandom;;
+    }
+    req.open("GET", callUrl, true);
+    req.send(null);
+}
+
+function requestPOST(url, query, req) {
+    req.open("POST", url, true);
+    req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    req.send(query);
+}
+
